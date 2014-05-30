a6b4734f1277ca95753198069ca833012ded7c35
Browenchen <owenchen@berkeley.edu>
2014-05-30 16:19:40 -0700

    web analytics
    this was pointless

---
+++ CodePractice/growth.py
@@ -0,0 +1,22 @@
+def check_growth(d1, d2):
+    # The line below contains the solution to this challenge.
+    # Consider it a small gift :-)
+    #
+    # Uncomment it and either:
+    #
+    #  - "Run a simple test" to run the code with a simple input
+    #    (see "Edit simple test")
+    #
+    #  - "Evaluate solution" to complete the challenge and go to the next one.
+
+
+    print "Increase" if d1 <= d2 else "Decrease"
+
+def compute_active_users(n, b):
+    # Write your code here
+    # To print results to the standard output you can use print
+    # Example: print "Hello world!"
+    per
+    print int(n - n * (b*.01))
+
+
