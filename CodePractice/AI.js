332137dc2cef92730e7addd954d2208edad22761
Browenchen <owenchen@berkeley.edu>
2014-06-12 11:56:23 +0100

    New Ideas
    s

---
+++ CodePractice/AI.js
@@ -0,0 +1,36 @@
+switch (Math.floor(Math.random()”10001)) ( 
+case 0: 
+    camaswer: ” “Look… we can talk about that, or we can address the real problems of hard working families.” ; 
+break 
+case 1: 
+     camanswer: ” “That’s not the question. The question is ‘Are we doing enough to help hard working families?’” ; 
+break 
+case 2: 
+    camanswer: ” “Listen to my list of three points while I wave my hands about.” ; 
+break 
+case 3: 
+    camanswer: ” “Look… we live in a democracy and I totally support your right to ask that.” ; 
+break 
+case 4: 
+    camanswer: ” “When we first came into power… we said things… and that’s something we should be proud of.” ; 
+break 
+case 5 
+    camanswer: ” “I was in some town last week. I met a man/woman just like you. He/she agreed with me.” ; 
+break 
+case 6 
+    camanswer: ” “I’m sorry — I can’t discuss my friends as it might affect all their court cases.” ; 
+break 
+case 7 
+    camanswer: ” “You have to wonder what’s wrong with Alex James, really.” ; 
+break 
+case 8 
+    camanswer: ” “My advisors wrote out a hashtag for me. What’s a hashtag? Also, what’s an internet?” ; 
+break 
+case 9 
+    camanswer: ” “Grrr Terrorists! Paedophiles!” ; 
+break 
+case 10 
+    camanswer: I once called Barack Obama ‘Daddy’, and Vladamir made fun of me.” ; 
+break 
+default 
+    camanswer: ” “Look… but what I will say is hard working families.” ; 
