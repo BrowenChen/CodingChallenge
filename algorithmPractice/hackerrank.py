9453e77710bad38baec9dcb7efaddf46c38c13e0
Browenchen <owenchen@berkeley.edu>
2014-09-24 09:50:35 -0700

    added quick sort

--- algorithmPractice/hackerrank.py
+++ algorithmPractice/hackerrank.py
@@ -120,9 +120,24 @@ class Graph:
 # Sort the input in O(nlogn) by increasing x values
 # Find the max weight in the sorted array
 
-def sortingAlg(arr):
-	#Sort the array
-	
+def quick_sort(items):
+    
+    if len(items) > 1:
+        pivot_index = len(items) / 2
+        smaller_items = []
+        larger_items = []
+
+        for i, val in enumerate(items):
+            if i != pivot_index:
+                if val < items[pivot_index]:
+                    smaller_items.append(val)
+                else:
+                    larger_items.append(val)
+
+        quick_sort(smaller_items)
+        quick_sort(larger_items)
+        items[:] = smaller_items + [items[pivot_index]] + larger_items
+
 
 def subweighting(sortedArr):
 	if len(sortedArr) == 1:
